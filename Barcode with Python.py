447efcde656d3d053856476a32c1d196e4e9d8f7
Divu <mavadiyadivyesh56@gmail.com>
2021-06-14 22:53:17 +0530

    Changed in barcode with python and new one tic tac toe added

--- Barcode with Python.py
+++ Barcode with Python.py
@@ -2,6 +2,7 @@
 
 
 import pyqrcode
+import png
 from pyqrcode import QRCode
 
 # String which represent the QR code
@@ -13,3 +14,4 @@ url = pyqrcode.create(s)
 # Create and save the png file naming ".png"
 url.svg("eduyear.jpeg", scale=8)
 
+url.png('myqr.png', scale = 6)
